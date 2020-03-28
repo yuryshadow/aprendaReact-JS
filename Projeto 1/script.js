bce122f6e8301e8f9be1fb996b618d9640b12c25
Yuri Carlos <yuryshadow@gmail.com>
2020-03-28 17:05:02 -0300

    Temporicacao

--- Projeto 1/script.js
+++ Projeto 1/script.js
@@ -1,32 +1,11 @@
-//Switch
-
-document.write("<br> Escolha seu pedido: </br>");
-document.write("<br> 0 - Sorvete / 1 - Suco</br>");
-document.write("<br> 2 - Coca cola / 3 - Agua gelada </br><br>");
-
-
-function pedir(){
-    
-    x = prompt("O que deseja pedir?");
-
-    switch(x){
-    
-        case "0":
-            alert("Voce pediu sorvete !");
-            break;
-        case "1":
-            alert("Voce pediu um suco !");
-            break;
-        case "2":
-            alert("Voce pediu uma coca gelada !");
-            break;
-        case "3":
-             alert("Voce pediu uma agua natural !");
-             break;
-        default:
-            alert("Nao temos essa opcao !");
-            break;
-    
-    }
-
+function acao(){
+    document.write("Executando... <br>");
 }
+
+
+// Executa de tempo em tempo
+var time = setInterval (acao, 1000);
+
+
+
+//setTimeout(acao, 3000);
