6c6876e5db4e07de273d6b3cf1b9aa3aab06c109
Yuri Carlos <yuryshadow@gmail.com>
2020-03-28 15:29:02 -0300

    Criando funcoes

--- Projeto 1/script.js
+++ Projeto 1/script.js
@@ -1,11 +1,23 @@
 
+// funcao entrar
 
-// Este eh o meu nome
-//var nome = "Yuri";
+function entrar(){
 
-/*
-Este
-eh
-meu
-nome
-*/
+    var area = document.getElementById('area');
+    var texto = prompt('Digite seu nome?');
+
+    if(texto == '' || texto == null){
+        alert('Digite seu nome novamente!');
+        area.innerHTML = 'Bem vindo ...';
+    }else{
+        area.innerHTML = 'Bem vindo' + texto;
+    }
+}
+
+function entrar2(nome, idade){
+
+    var area = document.getElementById('area2');
+    var texto = prompt('Digite seu sobrenome?');
+    area.innerHTML = nome + " " + texto + " " + idade;
+
+}
